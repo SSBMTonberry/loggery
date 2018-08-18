631c73a68be10d95349d04eef0b607bf86d87494
Robin Pettersen <ssbmtonberry@gmail.com>
2018-08-18 23:15:37 +0200

    Got a basic CMake-project linked with all the dependencies needed
    A working CMake-project building a basic example application with ImGui and GLFW working, including a simple "key_callback" function, handling the escape key to exit.
    
    TODO: Find a neat way to make it a bit more responsive which does not include glfwSwapInterval(0), that will put the application on steroids.

---
+++ main_dummy.cpp
@@ -0,0 +1,10 @@
+//
+// Created by robin on 18.08.18.
+//
+#include <fmt/core.h>
+#include <fmt/format.h>
+
+int main(int argc, char **argv, char** envp)
+{
+    std::string strtest = fmt::format("Hello");
+}
