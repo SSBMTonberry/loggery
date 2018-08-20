99ea5c8357a155e09dc0c6e5094387130087e9f1
Robin Pettersen <ssbmtonberry@gmail.com>
2018-08-20 07:41:08 +0200

    Added libraries for Windows (MSVC) and CMake-config (LoggerConfig.h.in)
    Some changes done in the CMakeLists.txt-file, but some are also missing. Windows will not be able to buiuld, yet.
    
    TODO: Compile via MinGW as well

---
+++ LoggeryConfig.h
@@ -0,0 +1,14 @@
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 1
+#define VERSION_PATCH 0
+#define LINUX 0
+#define WINDOWS 1
+#define APPLE 0
+#define MSVC 0
+#define MINGW 0
+#define CMAKE_MAKE_PROGRAM nmake
+#define CMAKE_SYSTEM_NAME Windows
+#define CMAKE_HOST_SYSTEM_NAME Windows //Windows, Linux or Darwin (OS X)
+#define CMAKE_HOST_SYSTEM_PROCESSOR AMD64
+#define CMAKE_CXX_COMPILER C:/Program Files (x86)/Microsoft Visual Studio/2017/Enterprise/VC/Tools/MSVC/14.14.26428/bin/Hostx86/x86/cl.exe
+#define CMAKE_CXX_COMPILER_VERSION 19.14.26428.1
